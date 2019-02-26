47d489dfd91bae19e897f5985db415cf68e8d849
kavindasilva <kss.kavinda@gmail.com>
2019-02-26 16:10:39 +0530

    http module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,5 @@ import { Component } from '@angular/core';
   `*/
 })
 export class AppComponent {
-	public name="nameStr";
-	public propSuc="p-suc";
-	private txt2="id-txt2";
-	private txt3="id-txt3";
-	private txt4="id-txt4";
-
-	public isDisabled=false;
-
-	private boolVal=true;
-	private special=true;
-	private special2=false;
-	private special3=3;
-
-	private msgObj={
-		"p-suc": !this.boolVal,
-		"p-italic": this.boolVal,
-		"p-20": this.special
-	};
-
-	private styleObj={
-		color: "blue",
-		fontStyle: "italic"
-	};
-
-  	title = 'a7t2 from a7t1';
-  	var1="VAR";
-
-  	eveClick(event){
-  		this.var1+=" hi";
-  		console.log(event);
-  	}
+	public title="a7t2 new"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Tut2Component } from './tut2/tut2.component';
+import { BasicsComponent } from './basics/basics.component';
+import { Tut3Component } from './tut3/tut3.component';
+import { TutServiceService } from './tut-service.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Tut2Component
+    Tut2Component,
+    BasicsComponent,
+    Tut3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    TutServiceService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
